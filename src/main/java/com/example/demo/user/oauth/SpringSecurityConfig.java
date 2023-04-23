7f06e81748edd123537f359be507398da2ca4ac6
vah72 <hoangvananh987@Gmail.com>
2023-04-23 14:19:53 +0700

    update order api

--- src/main/java/com/example/demo/user/oauth/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/user/oauth/SpringSecurityConfig.java
@@ -5,15 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig{
+public class SpringSecurityConfig {
     @Autowired
     private UserJwtService userJwtService;
     @Autowired
@@ -29,7 +27,4 @@ public class SpringSecurityConfig{
         ;
         return http.build();
     }
-
-
-
 }
